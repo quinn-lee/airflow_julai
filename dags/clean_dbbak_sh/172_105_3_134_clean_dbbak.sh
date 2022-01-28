f5c8bf959332ae175fc4c8e32538c8eeec5c54f9
lifuyuan <lifuyuan33@gmail.com>
2022-01-28 10:18:33 +0800

    n

--- dags/clean_dbbak_sh/172_105_3_134_clean_dbbak.sh
+++ dags/clean_dbbak_sh/172_105_3_134_clean_dbbak.sh
@@ -9,13 +9,13 @@ for db_bak_dir in `ls $db_bak_path`
 do 
 	if test -d $db_bak_dir ; then
 		#保留3天数据,校验是否存在后一天的记录
-		check_time=`date -d "-3 day" +%Y%m%d`
+		check_time=`date -d "-7 day" +%Y%m%d`
 		check_file="${db_bak_path}/${db_bak_dir}/*${check_time}*txt.gz"
 		ls $check_file > /dev/null
 		if [ $? -ne 0 ] ; then
 			echo "no next file can not clean"
 		else
-			rm_time=`date -d "-3 day" +%Y%m%d`
+			rm_time=`date -d "-7 day" +%Y%m%d`
 			rm_file="${db_bak_path}/${db_bak_dir}/*${rm_time}*"
 			echo "rm ${rm_file}"
 			rm $rm_file
